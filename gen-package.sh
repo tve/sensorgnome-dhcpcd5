e6e76c9cbbf6e6defcce98caa0b11c5c2488d0f4
Thorsten von Eicken <tve@voneicken.com>
2023-03-11 11:23:01 -0800

    dhcpcd-9.4.1 for rpi

---
+++ gen-package.sh
@@ -0,0 +1,22 @@
+#! /bin/bash -e
+export DESTDIR=build-temp
+rm -rf $DESTDIR
+mkdir $DESTDIR
+
+# create dockcross script
+echo "Generating dockercross script"
+IMG=tvoneicken/sensorgnome-dockcross:armv7-rpi-bullseye-main
+docker run $IMG >sensorgnome-dockcross
+chmod +x sensorgnome-dockcross
+
+echo "Cross-compiling and installing"
+./sensorgnome-dockcross -i $IMG ./update-dhcpcd5.sh $DESTDIR
+#DESTDIR=$DESTDIR STRIP=armv7-unknown-linux-gnueabi-strip
+
+# Boilerplate package generation
+# cp -r DEBIAN $DESTDIR
+# sed -e "/^Version/s/:.*/: $(date +%Y.%j)/" -i $DESTDIR/DEBIAN/control # set version: YYYY.DDD
+# mkdir -p packages
+# dpkg-deb --root-owner-group --build $DESTDIR packages
+# dpkg-deb --contents packages
+ls -lh packages
---
+++ update-dhcpcd5.sh
@@ -0,0 +1,45 @@
+#! /bin/bash -ex
+cd $1
+pwd
+
+RPI_SRC=dhcpcd5_8.1.2-1+rpt9.debian.tar.xz
+UP_SRC=dhcpcd-9.4.1.tar.xz
+NEW_SRC=dhcpcd5-9.4.1
+
+# We need uupdate, that pulls in a ton of crap, oh well, hope not to need to run this too many times
+sudo apt-get update
+sudo apt-get install -y devscripts libudev-dev
+
+# Fix-up stuff not properly installed by sensorgnome-dockcross
+for f in libudev.h; do
+  sudo ln -s /usr/include/$f /usr/xcc/armv7-unknown-linux-gnueabi/armv7-unknown-linux-gnueabi/sysroot/usr/include/
+done
+
+# Download Raspberry Pi's dhcpcd5 source package
+wget -nv http://archive.raspberrypi.org/debian/pool/main/d/dhcpcd5/$RPI_SRC
+mkdir dhcpcd5-old
+tar -C dhcpcd5-old -Jxf $RPI_SRC
+
+# Download upstream source package
+wget -nv -O $UP_SRC https://roy.marples.name/downloads/dhcpcd/$UP_SRC
+
+# Update the rPi sources with the upstream
+cd dhcpcd5-old
+echo "yes" | uupdate --verbose ../$UP_SRC
+
+# Build the resulting new package
+cd ../$NEW_SRC
+# need some hack so dpkg-buildpackage succeeds
+#export DEB_BUILD_OPTIONS=nostrip
+#sudo rm /usr/bin/dh_dwz
+#sudo ln -s /bin/true /usr/bin/dh_dwz
+( cd  /usr/xcc/armv7-unknown-linux-gnueabi/bin;
+  for f in objcopy objdump strip; do sudo ln -s armv7-unknown-linux-gnueabi-$f arm-linux-gnueabihf-$f; done;
+)
+#
+# echo dpkg-buildpackage -us -uc -nc -d --target-arch armhf -a armhf --target-type armv7-unknown-linux-gnueabi
+# bash -i
+#
+dpkg-buildpackage -us -uc -nc -d --target-arch armhf -a armhf --target-type armv7-unknown-linux-gnueabi
+
+cp ../${NEW_SRC/-/_}*.deb ../../packages
